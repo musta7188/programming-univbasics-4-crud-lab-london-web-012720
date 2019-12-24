a3f9b9381f6eccdc748ef60bd7f44eb6fabea60f
Moustafa Mohamed <musta7188@hotmail.it>
2019-12-24 22:53:32 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -46,6 +46,10 @@ end
 
 def retrieve_element_from_index(array, index_number)
 
+  retrieve_element = array[index_number]
+
+  retrieve_element
+
 end
 
 def retrieve_first_element_from_array(array)
